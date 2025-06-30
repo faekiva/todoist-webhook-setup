18aaefa7886870670507a3313c971f6c0afa5b18
kiva <git@kiva.lgbt>
2025-06-30 00:28:00 -0500

    working webhook setup

--- todoist-webhook-setup.sh
+++ todoist-webhook-setup.sh
@@ -3,8 +3,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
-
 if ! command -v gum &>/dev/null; then
     echo "gum is not installed. Please install it."
 fi
@@ -13,22 +11,26 @@ if ! command -v httpie &>/dev/null; then
     echo "httpie is not installed. Please install it."
 fi
 
-gum confirm "This script will set up a Todoist webhook. Do you want to continue?" || exit 1
-
 if [[ -z "$CLIENT_ID" ]]; then
     echo "$CLIENT_ID"
     CLIENT_ID=$(gum input --placeholder "Enter your Todoist Client ID")
+    export CLIENT_ID
 fi
 
 if [[ -z "$CLIENT_SECRET" ]]; then
     echo "$CLIENT_SECRET"
     CLIENT_SECRET=$(gum input --placeholder "Enter your Todoist Client Secret")
+    export CLIENT_SECRET
 fi
 
-RANDOM_STRING=$(
+STATE_STRING=$(
     tr -dc A-Za-z0-9 </dev/urandom | head -c 13
     echo
 )
-echo "https://todoist.com/oauth/authorize?client_id=$CLIENT_ID&scope=data:read_write,task:add,data:delete&state=$RANDOM_STRING"
 
-todoist-oauth-receiver
+export STATE_STRING
+
+echo "Please visit the following URL in your browser to authorize the application:"
+echo "https://todoist.com/oauth/authorize?client_id=$CLIENT_ID&scope=data:read_write,task:add,data:delete&state=$STATE_STRING"
+
+gum spin --spinner dot --title "waiting for request..." --show-output -- todoist-oauth-receiver
--- web-server/main.go
+++ web-server/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"sync"
 )
 
+func mustGetenv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatal("Environment variable " + key + " is not set")
+	}
+	return value
+}
+
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Query().Get("code")
 		state := r.URL.Query().Get("state")
@@ -16,9 +29,23 @@ func main() {
 			http.Error(w, "Missing state parameter", http.StatusBadRequest)
 			return
 		}
-		// write the code and state to a file.
+		if state != os.Getenv("STATE_STRING") {
+			http.Error(w, "Invalid state parameter", http.StatusBadRequest)
+			return
+		}
+		resp, err := http.Post("https://todoist.com/oauth/access_token?client_id="+mustGetenv("CLIENT_ID")+"&client_secret="+mustGetenv("CLIENT_SECRET")+"&code="+code, "application/x-www-form-urlencoded", nil)
+		if err != nil {
+			http.Error(w, "Failed to get access token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if resp.StatusCode >= 400 {
+			http.Error(w, "Failed to get access token: "+resp.Status, resp.StatusCode)
+			return
+		}
 
-		w.Write([]byte("Success!"))
+		w.Write([]byte("Webhooks initialized!"))
+		wg.Done()
 	})
-	http.ListenAndServe(":3055", nil)
+	go http.ListenAndServe(":3055", nil)
+	wg.Wait()
 }
